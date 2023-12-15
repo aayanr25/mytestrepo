1f20dba17bf6f996f3e99a35ec3feeecd62fa50d
BT_1N3_30 <sample@bths.edu>
2023-12-15 08:57:32 -0500

    added SomeClass

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main (String[] args) {
         System.out.println("First shared repo");
         System.out.println("this is the other partner adding some code");
+        System.out.println("The third change");
     }
 }
