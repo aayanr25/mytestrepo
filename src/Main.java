1881d13d26729862bfb7999fb984c8eaaf667fed
BT_1N3_30 <sample@bths.edu>
2023-12-15 09:01:08 -0500

    updated main

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("First shared repo");
         System.out.println("this is the other partner adding some code");
         System.out.println("The third change");
+        System.out.println("What's up, its the fourth change");
     }
 }
